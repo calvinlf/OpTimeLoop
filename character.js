0aee546a9b2478f5371a244a1f533eb3cb6dd10c
calvinlf <calvin.l.fischer@gmail.com>
2019-09-04 09:13:22 -0600

    worked on html version of game, still in brainstorming stage...

---
+++ character.js
@@ -0,0 +1,13 @@
+exports.character = {
+    items: [
+        "time-watch"
+    ],
+    events: [],
+    charisma: 5,
+    perception: 2,
+    resets: 0,
+    light: 0,
+    attributes: [
+        "hungry"
+    ],
+}
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const fs = require('fs')
+const express = require('express')
+const app = express()
+
+app.use('/', (req, res, next) => {
+    console.log(`Request Type: ${req.method} called on path ${req.path}`)
+    next()
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/home.html')
+})
+
+app.get('/next', function (req, res) {
+    res.sendFile(__dirname+'/first.html')
+})
+
+app.listen(3000, () => {
+    console.log('Beginning server')
+    console.log('    [INFO] Server running on port: ' + 3000)
+})
